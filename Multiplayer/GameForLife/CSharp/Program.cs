d8a1fe387b2ae82e9b680b93b1a0a521bc13106f
Kristaps Dzērve-Štrāls <kristaps@kriss.tech>
2019-03-20 11:14:44 +0200

    game for life 1

---
+++ Multiplayer/GameForLife/CSharp/Program.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace GameForLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs;
+            int projectCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < projectCount; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int a = int.Parse(inputs[0]);
+                int b = int.Parse(inputs[1]);
+                int c = int.Parse(inputs[2]);
+                int d = int.Parse(inputs[3]);
+                int e = int.Parse(inputs[4]);
+            }
+
+            // game loop
+            while (true)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    inputs = Console.ReadLine().Split(' ');
+                    string target = inputs[0];
+                    int eta = int.Parse(inputs[1]);
+                    int score = int.Parse(inputs[2]);
+                    int storageA = int.Parse(inputs[3]);
+                    int storageB = int.Parse(inputs[4]);
+                    int storageC = int.Parse(inputs[5]);
+                    int storageD = int.Parse(inputs[6]);
+                    int storageE = int.Parse(inputs[7]);
+                    int expertiseA = int.Parse(inputs[8]);
+                    int expertiseB = int.Parse(inputs[9]);
+                    int expertiseC = int.Parse(inputs[10]);
+                    int expertiseD = int.Parse(inputs[11]);
+                    int expertiseE = int.Parse(inputs[12]);
+                }
+                inputs = Console.ReadLine().Split(' ');
+                int availableA = int.Parse(inputs[0]);
+                int availableB = int.Parse(inputs[1]);
+                int availableC = int.Parse(inputs[2]);
+                int availableD = int.Parse(inputs[3]);
+                int availableE = int.Parse(inputs[4]);
+                int sampleCount = int.Parse(Console.ReadLine());
+                for (int i = 0; i < sampleCount; i++)
+                {
+                    inputs = Console.ReadLine().Split(' ');
+                    int sampleId = int.Parse(inputs[0]);
+                    int carriedBy = int.Parse(inputs[1]);
+                    int rank = int.Parse(inputs[2]);
+                    string expertiseGain = inputs[3];
+                    int health = int.Parse(inputs[4]);
+                    int costA = int.Parse(inputs[5]);
+                    int costB = int.Parse(inputs[6]);
+                    int costC = int.Parse(inputs[7]);
+                    int costD = int.Parse(inputs[8]);
+                    int costE = int.Parse(inputs[9]);
+                }
+
+                // Write an action using Console.WriteLine()
+                // To debug: Console.Error.WriteLine("Debug messages...");
+
+                Console.WriteLine("GOTO DIAGNOSIS");
+            }
+        }
+    }
+
+    class Robot
+    {
+        public List<Molecule> Molecules { get; set; } = new List<Molecule>();
+        public List<SampleData> SampleDatas { get; set; } = new List<SampleData>();
+
+        public int SampleDataCount => SampleDatas.Count;
+        public int MoleculeCount => Molecules.Count;
+    }
+
+    class SampleData
+    {
+        public SampleData()
+        {
+            Molecules = new List<Molecule>();
+        }
+
+        public List<Molecule> Molecules { get; private set; }
+    }
+
+    class Molecule
+    {
+        public Molecule(string type)
+        {
+            Type = type;
+        }
+
+        public string Type { get; private set; }
+    }
+
+    static class MoleculeType
+    {
+        public const string A = "A";
+        public const string B = "B";
+        public const string C = "C";
+        public const string D = "D";
+        public const string E = "E";
+    }
+}
